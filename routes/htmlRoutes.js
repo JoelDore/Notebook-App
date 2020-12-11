05fda22cce4cf0e2ee25ae1a08c29af53b7cf10e
JES20 <dore.joel.dore@gmail.com>
2020-12-11 10:09:27 -0500

    🎞️ Add demo GIF

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,11 @@ module.exports = (app) => {
     // Handle html GET requests:
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'))
-        console.log(`GET /notes`)
+        console.log('GET /notes')
     })
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
-        console.log(`GET ${req.url}`)
     })
 
 }
